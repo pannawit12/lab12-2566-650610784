21cbefc9c8eea2c38a2f6ead31c9bf9ecc2fdada
Your Name <you@example.com>
2023-08-03 15:06:34 +0700

    fish

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ export default function HomePage() {
           <Controller />
         </LangContext.Provider>
       </ThemeContext.Provider>
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+      <Footer year="2023" fullName="Pannawit Setsiriwanit" studentId="650610784" />
     </div>
   );
 }
